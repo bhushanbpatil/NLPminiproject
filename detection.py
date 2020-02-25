54d5c3738a77318987cb80de8f9eaa906806efb5
bhushan patil <bhushanbpatil8@gmail.com>
2020-02-25 19:08:49 +0530

    bharat maata ki jai

--- detection.py
+++ detection.py
@@ -1,3 +1,7 @@
-from inltk.inltk import *
-x=identify_language('मरठभष ह इड-यरपय भषकलतल एक भष आह. मरठ भष  वय शतकपसन परचलत असन, मरठ भषच नरमत ससकतपसन झललय महरषटर')
-print('s:',x)
+from langdetect import detect
+output= detect('ભારત રત્ન ભારત દેશનો સર્વોચ્ચ નાગરિક સન્માન પુરસ્કાર છે.')
+print(output)
+
+# from inltk.inltk import *
+# x=identify_language('मरठभष ह इड-यरपय भषकलतल एक भष आह. मरठ भष  वय शतकपसन परचलत असन, मरठ भषच नरमत ससकतपसन झललय महरषटर')
+# print('s:',x)
